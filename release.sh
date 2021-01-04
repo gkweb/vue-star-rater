f5562aa07c42ddaa7e1d62a2300b1ae4a5c7c91d
Glade <gladekettle@gmail.com>
2021-01-04 23:02:24 +1100

    0.0.8

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Bump and release"
+npm run build
+npm version patch --force
+npm publish
+
+echo "Install latest vue-star-rater in docs"
+cd docs/
+npm i vue-star-rater@latest
+git add package*
+git commit -m "Bump to vue-star-rater@latest"
